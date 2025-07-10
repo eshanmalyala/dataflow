e79e12c01a8cf72b6a2c63dd60cb9367e2f8395a
eshanmalyala <eshanmalyala@outlook.com>
2025-07-10 11:52:57 +0530

    Update launch_template.sh

--- Project/dataflow/streaming/launch_template.sh
+++ Project/dataflow/streaming/launch_template.sh
@@ -1,1 +1,8 @@
-#
+#!/bin/bash
+gcloud dataflow flex-template build gs://getwellsoon-bucket/templates/streaming_template.json \
+  --image gcr.io/gcp-agent-garden/dataflow-streaming-pii \
+  --sdk-language "PYTHON" \
+  --flex-template-base-image "PYTHON" \
+  --metadata-file metadata.json \
+  --python-module streaming_pipeline \
+  --temp-location gs://getwellsoon-bucket/tmp
