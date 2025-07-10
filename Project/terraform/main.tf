bdd8df1c2cf2f649e6f414b598ec4cc3f5d273bf
eshanmalyala <eshanmalyala@outlook.com>
2025-07-10 12:00:59 +0530

    Update main.tf

--- Project/terraform/main.tf
+++ Project/terraform/main.tf
@@ -1,1 +1,32 @@
-#
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_pubsub_topic" "topic" {
+  name = var.topic_id
+}
+
+resource "google_pubsub_subscription" "sub" {
+  name  = "${var.topic_id}-sub"
+  topic = google_pubsub_topic.topic.id
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = var.bucket_name
+  location = var.region
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = var.dataset_id
+  location                    = var.region
+  delete_contents_on_destroy = true
+}
+
+resource "google_artifact_registry_repository" "repo" {
+  provider      = google
+  location      = var.region
+  repository_id = var.artifact_registry_name
+  description   = "Artifact Registry for Dataflow Flex Templates"
+  format        = "DOCKER"
+}
