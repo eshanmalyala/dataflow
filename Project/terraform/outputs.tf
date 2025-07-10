0a2024cb7f8da896f5e6302177162efd70a7cfad
eshanmalyala <eshanmalyala@outlook.com>
2025-07-10 12:01:23 +0530

    Update outputs.tf

--- Project/terraform/outputs.tf
+++ Project/terraform/outputs.tf
@@ -1,1 +1,11 @@
-#
+output "pubsub_topic" {
+  value = google_pubsub_topic.topic.name
+}
+
+output "artifact_registry" {
+  value = google_artifact_registry_repository.repo.name
+}
+
+output "bq_dataset" {
+  value = google_bigquery_dataset.dataset.dataset_id
+}
